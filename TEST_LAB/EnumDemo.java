65ad60139466ba9efd754d6432f394b4b2723ce9
samy <39716963+samykhelifa@users.noreply.github.com>
2019-08-25 16:43:51 +0200

    Update EnumDemo.java

--- TEST_LAB/EnumDemo.java
+++ TEST_LAB/EnumDemo.java
@@ -1,9 +1,15 @@
 
 import java.util.*;
 
-enum Apple {   Jonathan, GoldenDel, RedDel, Winesap, Cortland }  
+enum Apple {   
+      Jonathan(10), GoldenDel(9), RedDel(12), Winesap(15), Cortland(8);    
+       private int price; // price of each apple    
+     // Constructor   
+      Apple(int p) { price = p; }    
+      int getPrice() { return price; } 
+}
 
-class EnumDemo2 {  
+class EnumDemo {  
       public static void main(String args[])   {    
            Apple ap;  
            List<String> ls =  new ArrayList<>(); 
@@ -18,6 +24,15 @@ class EnumDemo2 {
         
       // use valueOf()     
         ap = Apple.valueOf("Winesap");    
-        System.out.println("ap contains " + ap);    
+        System.out.println("ap contains " + ap);   
+            
+        // Display price of Winesap.     
+            System.out.println("Winesap costs " + Apple.Winesap.getPrice() +   
+                               " cents.\n");      
+       // Display all apples and prices. 
+            System.out.println("All apple prices:");    
+            for(Apple a : Apple.values())       
+                  System.out.println(a + " costs " + a.getPrice() +  
+                                     " cents."); 
 
 } }
