43c216c4e78e0fd6c2990c509d1537ce3880a77b
samy <39716963+samykhelifa@users.noreply.github.com>
2019-08-25 15:10:33 +0200

    Update EnumDemo.java

--- TEST_LAB/EnumDemo.java
+++ TEST_LAB/EnumDemo.java
@@ -1,1 +1,23 @@
 
+import java.util.*;
+
+enum Apple {   Jonathan, GoldenDel, RedDel, Winesap, Cortland }  
+
+class EnumDemo2 {  
+      public static void main(String args[])   {    
+           Apple ap;  
+           List<String> ls =  new ArrayList<>(); 
+           System.out.println("Here are all Apple constants:");      
+      // use values()     
+       Apple allapples[] = Apple.values();     
+        for(Apple a : allapples)  {
+          ls.add(a.toString().toUpperCase());
+          System.out.println(a); }     
+           System.out.println(ls);  
+        
+        
+      // use valueOf()     
+        ap = Apple.valueOf("Winesap");    
+        System.out.println("ap contains " + ap);    
+
+} }
